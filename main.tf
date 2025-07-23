782db05aba8a2fbb8405b2ee1153037653a81e18
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-23 08:20:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,4 +29,33 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 #
+# Public IPs (simulated reserved IPs)
+resource "azurerm_public_ip" "reserved" {
+  for_each = toset(["fw1", "fw2"])
 
+  name                = "avx-eastus-pa-${each.key}-pip"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# NICs that may or may not match the reserved IPs
+resource "azurerm_network_interface" "nic" {
+  for_each = toset(["fw1", "fw2"])
+
+  name                = "${each.key}-nic"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+  lifecycle {
+    ignore_changes = [
+      ip_configuration[0].public_ip_address_id
+    ]
+  }
+}
