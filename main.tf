849a3a3c46e27c4b1cdd03a1c47190fde58d8be6
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-24 16:39:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -114,7 +114,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   size                            = "Standard_B1s"
   #network_interface_ids           = [azurerm_network_interface.dummy_nic.id]
   network_interface_ids           = [azurerm_network_interface.nic.id]
-  depends_on = [azurerm_network_interface.dummy_nic]
+  #depends_on = [azurerm_network_interface.dummy_nic]
   admin_username                  = "azureuser"
   disable_password_authentication = true
   # 👇 Required dummy key – no login needed
