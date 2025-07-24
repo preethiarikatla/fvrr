b589d51c03bae0be0c5790b3982dc840651534b0
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-24 15:05:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,12 +37,8 @@ resource "azurerm_public_ip" "reserved" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   allocation_method   = "Static"
-  sku                 = "Standard"
-  lifecycle {
-    ignore_changes = [
-      ip_address
-    ]
-  }
+  sku                 = "Basic"
+
 }
 
 
