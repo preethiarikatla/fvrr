a91324ba8dc3dd630305af37ab55b85aa80961e4
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-30 09:46:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,147 +9,128 @@ terraform {
 provider "azurerm" {
   features {}
 }
-#
 
-resource "azurerm_resource_group" "rg" {
-  name     = "copilot-test-rg"
-  location = "East US"
+variable "location" {
+  default = "eastus2"
 }
 
-# Virtual Network and Subnet
+variable "resource_group_name" {
+  default = "demo-rg"
+}
+
+variable "lb_name" {
+  default = "demo-lb"
+}
+
+variable "ipconfig" {
+  default = "ipconfig1"
+}
+
+# RESOURCE GROUP
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# VNET + SUBNET
 resource "azurerm_virtual_network" "vnet" {
-  name                = "copilot-vnet"
+  name                = "demo-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "copilot-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  name                 = "demo-subnet"
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# NSG
-resource "azurerm_network_security_group" "nsg" {
-  name                = "copilot-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "allow-ssh"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  lifecycle {
-    ignore_changes = [tags]
-  }
-}
-
-# Public IP
-resource "azurerm_public_ip" "pip" {
-  name                = "copilot-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Basic"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
 # NIC
 resource "azurerm_network_interface" "nic" {
-  name                = "copilot-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = "demo-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
-  }
-
-  lifecycle {
-    ignore_changes = [tags]
-  }
-}
-
-# NIC + NSG Association
-resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "azurerm_network_interface" "dummy_nic" {
-  name                = "copilot-dummy-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
+    name                          = var.ipconfig
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
+}
 
-  lifecycle {
-    ignore_changes = [tags]
+# PUBLIC IP for LB
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.lb_name}-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# LOAD BALANCER
+resource "azurerm_lb" "lb" {
+  name                = var.lb_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "${var.lb_name}-frontend"
+    public_ip_address_id = azurerm_public_ip.pip.id
   }
 }
-# Linux VM
+
+# BACKEND POOL
+resource "azurerm_lb_backend_address_pool" "backend_pool" {
+  name                 = "${var.lb_name}-bepool"
+  loadbalancer_id      = azurerm_lb.lb.id
+  resource_group_name  = var.resource_group_name
+}
+
+# HEALTH PROBE
+resource "azurerm_lb_probe" "probe" {
+  name                = "${var.lb_name}-tcp-probe"
+  resource_group_name = var.resource_group_name
+  loadbalancer_id     = azurerm_lb.lb.id
+  protocol            = "Tcp"
+  port                = 80
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
+
+# LB RULE
+resource "azurerm_lb_rule" "rule" {
+  name                           = "${var.lb_name}-http"
+  resource_group_name            = var.resource_group_name
+  loadbalancer_id                = azurerm_lb.lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "${var.lb_name}-frontend"
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.backend_pool.id
+  probe_id                       = azurerm_lb_probe.probe.id
+  enable_tcp_reset               = true
+  disable_outbound_snat          = false
+}
+
+# BACKEND POOL ASSOCIATION
+resource "azurerm_network_interface_backend_address_pool_association" "nic_to_backend" {
+  network_interface_id    = azurerm_network_interface.nic.id
+  ip_configuration_name   = var.ipconfig
+  backend_address_pool_id = azurerm_lb_backend_address_pool.backend_pool.id
+}
+
+# VM (Linux Ubuntu) with SSH key only
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                            = "copilot-test-vm"
-  location                        = azurerm_resource_group.rg.location
-  resource_group_name             = azurerm_resource_group.rg.name
-  size                            = "Standard_B1s"
-  network_interface_ids           = [azurerm_network_interface.dummy_nic.id]
-  #network_interface_ids           = [azurerm_network_interface.nic.id]
-  depends_on = [azurerm_network_interface.dummy_nic]
-  admin_username                  = "azureuser"
-  disable_password_authentication = true
-  # 👇 Required dummy key – no login needed
-  admin_ssh_key {
-  username   = "azureuser"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRJaB9f+o1bWUQFfigorqJVfcLNKX2Ox29MtvqyPgMz4D/WuSpa09nIbgp195vuqLbHiGG0gV2WNQab1MOLbI8xSm9wLNyX0Srm4+jwWXylHpjflm3L1QnceQANnt2LVqr7h2mSMubytDxKhImOnSXejgylyVp+nFV0624lHuyJXDNHZl+RXC0giEE1Iujz3Mu2lyZ1DkWAYzAbvvZfu8jOVuSk8hdpjZn6k0jvMkBGbCNxyg18SM/TSgx5X5Mwszjbx2dU1tNpXfW87XcvRn9zVE7Asw196YoZHx2yRadEf1KCv+vJxW/6Pwu1V7Uqg4k2t58rJ46217l39ZlKUJ9 preethi@SandboxHost-638883515602013682"
-}
-source_image_reference {
-  publisher = "Canonical"
-  offer     = "UbuntuServer"
-  sku       = "18.04-LTS"
-  version   = "latest"
-}
+  name                = "demo-vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
-  os_disk {
-    name                 = "copilot-osdisk"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      tags,
-    ]
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "vm_new" {
-  name                            = "copilot-new-vm"
-  location                        = azurerm_resource_group.rg.location
-  resource_group_name             = azurerm_resource_group.rg.name
-  size                            = "Standard_B1s"
-  network_interface_ids           = [azurerm_network_interface.nic.id]  # original NIC
-  depends_on = [azurerm_linux_virtual_machine.vm] 
-  admin_username                  = "azureuser"
   disable_password_authentication = true
 
   admin_ssh_key {
@@ -157,20 +138,25 @@ resource "azurerm_linux_virtual_machine" "vm_new" {
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRJaB9f+o1bWUQFfigorqJVfcLNKX2Ox29MtvqyPgMz4D/WuSpa09nIbgp195vuqLbHiGG0gV2WNQab1MOLbI8xSm9wLNyX0Srm4+jwWXylHpjflm3L1QnceQANnt2LVqr7h2mSMubytDxKhImOnSXejgylyVp+nFV0624lHuyJXDNHZl+RXC0giEE1Iujz3Mu2lyZ1DkWAYzAbvvZfu8jOVuSk8hdpjZn6k0jvMkBGbCNxyg18SM/TSgx5X5Mwszjbx2dU1tNpXfW87XcvRn9zVE7Asw196YoZHx2yRadEf1KCv+vJxW/6Pwu1V7Uqg4k2t58rJ46217l39ZlKUJ9 preethi@SandboxHost-638883515602013682"
   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
   os_disk {
-    name                 = "copilot-new-osdisk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    name                 = "osdisk"
   }
 
-  lifecycle {
-    ignore_changes = [tags]
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
   }
+}
+
+# OUTPUTS
+output "vm_private_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
+}
+
+output "lb_public_ip" {
+  value = azurerm_public_ip.pip.ip_address
 }
