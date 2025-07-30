32494ca4b540d1cf75553f704de6807bf0208b6b
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-30 09:58:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,16 +98,14 @@ resource "azurerm_lb_probe" "probe" {
   number_of_probes    = 2
 }
 
-# LB RULE
 resource "azurerm_lb_rule" "rule" {
   name                           = "${var.lb_name}-http"
-  resource_group_name            = var.resource_group_name
   loadbalancer_id                = azurerm_lb.lb.id
   protocol                       = "Tcp"
   frontend_port                  = 80
   backend_port                   = 80
   frontend_ip_configuration_name = "${var.lb_name}-frontend"
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.backend_pool.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backend_pool.id]
   probe_id                       = azurerm_lb_probe.probe.id
   enable_tcp_reset               = true
   disable_outbound_snat          = false
