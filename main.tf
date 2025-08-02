c1e7061c98b9de11bda19067b1ac5bd14fe4cd6b
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-08-01 20:13:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "azurerm_network_interface" "egress" {
   resource_group_name = azurerm_resource_group.test.name
 
   ip_configuration {
-    name                          = "ipconfig22"
+    name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     # Note: No public_ip_address_id assigned here
