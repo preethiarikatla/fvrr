cecf747bdcc52dc6abc9090f95e887df1c12db77
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-08-03 14:48:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -105,7 +105,7 @@ resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
 #  }
 #}
 # Linux VM
-  resource "azurerm_linux_virtual_machine" "vm" {
+#  resource "azurerm_linux_virtual_machine" "vm" {
 #      name                            = "copilot-test-vm"
 #      location                        = azurerm_resource_group.rg.location
 #      resource_group_name             = azurerm_resource_group.rg.name
