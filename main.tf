fdfcebfad4cb2ed9dea6ab01ddd8fa91c85795c1
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-08-05 16:50:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -105,34 +105,34 @@ resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
 #  }
 #}
 # Linux VM
- # resource "azurerm_linux_virtual_machine" "vm" {
- #   name                            = "copilot-test-vm"
- #   location                        = azurerm_resource_group.rg.location
- #   resource_group_name             = azurerm_resource_group.rg.name
- #   size                            = "Standard_B1s"
- #  ##network_interface_ids           = [azurerm_network_interface.dummy_nic.id]
- #   network_interface_ids           = [azurerm_network_interface.nic.id]
- #  # depends_on = [azurerm_network_interface.dummy_nic]
- #   admin_username                  = "azureuser"
- #   disable_password_authentication = true
- #  # # 👇 Required dummy key – no login needed
- #   admin_ssh_key {
- #   username   = "azureuser"
- #   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRJaB9f+o1bWUQFfigorqJVfcLNKX2Ox29MtvqyPgMz4D/WuSpa09nIbgp195vuqLbHiGG0gV2WNQab1MOLbI8xSm9wLNyX0Srm4+jwWXylHpjflm3L1QnceQANnt2LVqr7h2mSMubytDxKhImOnSXejgylyVp+nFV0624lHuyJXDNHZl+RXC0giEE1Iujz3Mu2lyZ1DkWAYzAbvvZfu8jOVuSk8hdpjZn6k0jvMkBGbCNxyg18SM/TSgx5X5Mwszjbx2dU1tNpXfW87XcvRn9zVE7Asw196YoZHx2yRadEf1KCv+vJxW/6Pwu1V7Uqg4k2t58rJ46217l39ZlKUJ9 preethi@SandboxHost-638883515602013682"
- # }
- # source_image_reference {
- #   publisher = "Canonical"
- #   offer     = "UbuntuServer"
- #   sku       = "18.04-LTS"
- #   version   = "latest"
- # }
+  resource "azurerm_linux_virtual_machine" "vm" {
+    name                            = "copilot-test-vm"
+    location                        = azurerm_resource_group.rg.location
+    resource_group_name             = azurerm_resource_group.rg.name
+    size                            = "Standard_B1s"
+   ##network_interface_ids           = [azurerm_network_interface.dummy_nic.id]
+    network_interface_ids           = [azurerm_network_interface.nic.id]
+   # depends_on = [azurerm_network_interface.dummy_nic]
+    admin_username                  = "azureuser"
+    disable_password_authentication = true
+   # # 👇 Required dummy key – no login needed
+    admin_ssh_key {
+    username   = "azureuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRJaB9f+o1bWUQFfigorqJVfcLNKX2Ox29MtvqyPgMz4D/WuSpa09nIbgp195vuqLbHiGG0gV2WNQab1MOLbI8xSm9wLNyX0Srm4+jwWXylHpjflm3L1QnceQANnt2LVqr7h2mSMubytDxKhImOnSXejgylyVp+nFV0624lHuyJXDNHZl+RXC0giEE1Iujz3Mu2lyZ1DkWAYzAbvvZfu8jOVuSk8hdpjZn6k0jvMkBGbCNxyg18SM/TSgx5X5Mwszjbx2dU1tNpXfW87XcvRn9zVE7Asw196YoZHx2yRadEf1KCv+vJxW/6Pwu1V7Uqg4k2t58rJ46217l39ZlKUJ9 preethi@SandboxHost-638883515602013682"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
 
-  # os_disk {
-  #    name                 = "copilot-osdisk"
-  #    caching              = "ReadWrite"
-  #   storage_account_type = "Standard_LRS"
-  #  }
- #}
+   os_disk {
+      name                 = "copilot-osdisk"
+      caching              = "ReadWrite"
+     storage_account_type = "Standard_LRS"
+    }
+ }
 # Managed Data Disk
 resource "azurerm_managed_disk" "default" {
   name                 = "copilot-data-disk"
@@ -150,7 +150,33 @@ resource "azurerm_managed_disk" "default" {
     ]
   }
 }
+# Create storage account for backups
+resource "azurerm_storage_account" "storagebackup" {
+  name                     = "avxbackupsprod" # or dynamically: "avxbackups${var.environment}" if var.environment = "prod"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
+  network_rules {
+    default_action             = "Deny"
+    bypass                     = ["AzureServices"]
+    virtual_network_subnet_ids = [azurerm_subnet.subnet.id] # aligns with your "copilot-subnet"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      allow_nested_items_to_be_public
+    ]
+  }
+}
+
+# Create container inside storage account for controller backups
+resource "azurerm_storage_container" "backup" {
+  name                  = "controllerbackup"
+  storage_account_name  = azurerm_storage_account.storagebackup.name
+  container_access_type = "private"
+}
 # Attach Data Disk to VM
 #resource "azurerm_virtual_machine_data_disk_attachment" "default" {
 #  managed_disk_id    = azurerm_managed_disk.default.id
