42028d596cf55e0aa5953259993e9dab3dd144c0
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-10-18 18:34:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.106.0" 
+      version = "~> 3.115.0" 
     }
   }
 }
@@ -14,4 +14,18 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg_test" {
   name     = "rg-test-low-version"
   location = "East US"
+}
+
+# keep the existing RG from Commit A
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-rg-baseline-demo"
+  location = "East US"
+}
+
+# NEW resource that will fail at apply (but plan OK)
+# bogus principal_id ensures "Principal not found" or auth failure from ARM
+resource "azurerm_role_assignment" "will_fail_on_apply" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Reader"
+  principal_id         = "00000000-0000-0000-0000-000000000000"  # invalid principal
 }
