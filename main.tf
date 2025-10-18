c37305c567b5f5556a81e81f5df076fe942fd04d
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-10-18 19:06:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,16 +16,3 @@ resource "azurerm_resource_group" "rg_test" {
   location = "East US"
 }
 
-# keep the existing RG from Commit A
-resource "azurerm_resource_group" "rg" {
-  name     = "tf-rg-baseline-demo"
-  location = "East US"
-}
-
-# NEW resource that will fail at apply (but plan OK)
-# bogus principal_id ensures "Principal not found" or auth failure from ARM
-resource "azurerm_role_assignment" "will_fail_on_apply" {
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Reader"
-  principal_id         = "00000000-0000-0000-0000-000000000000"  # invalid principal
-}
