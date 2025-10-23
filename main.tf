20f6b824841eef1a3fa3465588b25c6924f98019
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-10-23 14:34:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,19 +4,36 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 2.50.0" 
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 1.13, != 1.13.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
 }
-
-resource "azurerm_resource_group" "rg_test" {
-  name     = "rg-test-low-version"
+# keep existing resources
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-rollback-demo"
   location = "East US"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-test-low"
-  location = "East US"
+resource "azapi_resource" "rg_tags" {
+  type      = "Microsoft.Resources/tags@2021-04-01"
+  name      = "default"
+  parent_id = azurerm_resource_group.rg.id
+  body = jsonencode({
+    properties = {
+      tags = { env = "v2" }
+    }
+  })
+}
+
+# NEW: this plans fine but will FAIL on apply (bogus principal)
+resource "azurerm_role_assignment" "will_fail_on_apply" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Reader"
+  principal_id         = "00000000-0000-0000-0000-000000000000"
 }
