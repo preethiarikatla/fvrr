1fdbe293b1be82439b2309dd29e816876ce35f19
pree <123841299+preethiarikatla@users.noreply.github.com>
2026-07-10 16:07:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,27 +11,51 @@ provider "azurerm" {
   features {}
 }
 
+# Change this to 1 or 2
+variable "vng_count" {
+  description = "Number of ExpressRoute VNGs to create"
+  type        = number
+  default     = 1
+
+  validation {
+    condition     = contains([1, 2], var.vng_count)
+    error_message = "vng_count must be either 1 or 2."
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-er-test"
   location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-er-test"
+  count = var.vng_count
+
+  name                = "vnet-er-test-${count.index + 1}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+
+  address_space = [
+    count.index == 0 ? "10.0.0.0/16" : "10.1.0.0/16"
+  ]
 }
 
 resource "azurerm_subnet" "gateway" {
+  count = var.vng_count
+
   name                 = "GatewaySubnet"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.255.0/27"]
+  virtual_network_name = azurerm_virtual_network.vnet[count.index].name
+
+  address_prefixes = [
+    count.index == 0 ? "10.0.255.0/27" : "10.1.255.0/27"
+  ]
 }
 
 resource "azurerm_virtual_network_gateway" "er_vng" {
-  name                = "vng-er-test"
+  count = var.vng_count
+
+  name                = "vng-er-test-${count.index + 1}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -41,6 +65,6 @@ resource "azurerm_virtual_network_gateway" "er_vng" {
   ip_configuration {
     name                          = "vnetGatewayConfig"
     private_ip_address_allocation = "Dynamic"
-    subnet_id                     = azurerm_subnet.gateway.id
+    subnet_id                     = azurerm_subnet.gateway[count.index].id
   }
 }
