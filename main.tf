8406087162deb0c13d472046d2466d1882b13b45
pree <123841299+preethiarikatla@users.noreply.github.com>
2026-07-10 17:47:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 variable "gateway_count" {
   description = "Number of ExpressRoute VNGs to create"
   type        = number
-  default     = 2
+  default     = 1
 
   validation {
     condition     = contains([1, 2], var.gateway_count)
@@ -52,20 +52,3 @@ resource "azurerm_subnet" "gateway" {
   ]
 }
 
-
-resource "azurerm_virtual_network_gateway" "er_vng" {
-  count = var.gateway_count
-
-  name                = "vng-er-test-${count.index + 1}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  type     = "ExpressRoute"
-  vpn_type = "RouteBased"
-  sku      = "UltraPerformance"
-
-  ip_configuration {
-    name                          = "vnetGatewayConfig"
-    subnet_id                     = azurerm_subnet.gateway[count.index].id
-  }
-}
