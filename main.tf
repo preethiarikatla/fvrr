2e506cec67f0239ce42a1742d92bf193d1435d24
pree <123841299+preethiarikatla@users.noreply.github.com>
2026-07-10 14:08:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,18 +10,6 @@ terraform {
 provider "azurerm" {
   features {}
 }
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 2.50.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = "rg-er-test"
